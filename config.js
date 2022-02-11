0359e56fefea42ff20b4b5033ea9d273ad9648fa
Poonam-Lunawat <68240432+Poonam-Lunawat@users.noreply.github.com>
2022-02-11 13:22:57 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAgXK1RMoQJrPMZjyN2FDOnDCmgyw5WH-c",
+  authDomain: "recipe-app-e5d77.firebaseapp.com",
+  databaseURL: "https://recipe-app-e5d77-default-rtdb.firebaseio.com",
+  projectId: "recipe-app-e5d77",
+  storageBucket: "recipe-app-e5d77.appspot.com",
+  messagingSenderId: "514770861327",
+  appId: "1:514770861327:web:936855f897e9ef196fd27a"
+};
